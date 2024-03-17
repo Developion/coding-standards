764a5b7a98aba2d2fd8aee7113f8077a6f43b5fe
MarcusGaius <marko.gajic@developion.com>
2024-03-17 13:06:11 -0100

    fix class name

--- src/SetList.php
+++ src/SetList.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Developion\CodingStandards;
 
-final class StrictTypesFixer
+final class SetList
 {
 	public const DEVELOPION = __DIR__ . '/../sets/developion.php';
 }
